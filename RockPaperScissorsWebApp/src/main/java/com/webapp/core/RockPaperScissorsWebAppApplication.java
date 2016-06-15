1d531be1d16d0c1a94ab08456225f04de361f9da
ozzman02 <osantamaria@gmail.com>
2016-06-15 17:27:07 -0600

    Creating basic Spring Boot project structure

---
+++ RockPaperScissorsWebApp/src/main/java/com/webapp/core/RockPaperScissorsWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.webapp.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorsWebAppApplication.class, args);
+	}
+}
---
+++ RockPaperScissorsWebApp/src/main/java/com/webapp/core/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.webapp.core;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RockPaperScissorsWebAppApplication.class);
+	}
+
+}
